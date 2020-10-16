82fea537e1ff5681e322d943f0190d8f8e42d8e8
Aurelien <aurelien@jsoft.biz>
2020-10-16 12:58:53 +0200

    bump up to new version fix release scripts

--- release.sh
+++ release.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+#
+# Copyright (c) 2014-2020 Aurélien Broszniowski
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 update() {
   sed -i bak -e s/$1-SNAPSHOT/$1/g pom.xml
   git commit -m "release $1"
@@ -14,12 +30,12 @@ fi
 
 update $1
 
-for name in `ls -d */`
+for name in `find . -name pom.xml -exec dirname {} \;`
 do
   echo "Working on $name"
   cd $name
   update $1
-  cd ..
+  cd -
   echo "---------------"
 done
 
